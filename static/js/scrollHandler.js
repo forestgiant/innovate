50169b4ff67fe6a01053bdb4495517c023744cbb
Matt Adams <matt@forestgiant.com>
2019-01-17 11:22:27 -0500

    Working draft of Validation section. Needs a reduction in height.

--- static/js/scrollHandler.js
+++ static/js/scrollHandler.js
@@ -1,116 +1,99 @@
 window.onload = function(fn, wait) {
-  let controller = new ScrollMagic.Controller(), 
-      offset = window.innerHeight + document.getElementById('text-set-02').clientHeight,
-      bodyTextOffset = window.innerHeight - document.getElementById('text-set-01').clientHeight;
-
-  
-
-  new ScrollMagic.Scene({
-    duration: 6500,
-    offset: 0,
-    })
-  .setPin('#image-set-01')
-  .addTo(controller);
-  
-  //---------------------------------------------------------
-  // Text Handling
-  //---------------------------------------------------------
-  new ScrollMagic.Scene({
-    duration: 6500,
-    offset: 0,
-    })
-  .setPin('#text-set-01', { pushFollowers: false })
-  .addTo(controller);
-
-  new ScrollMagic.Scene({
-    duration: offset,
-    offset: -1 * offset,
-  })
-  .setPin('#text-set-02')
-  .addTo(controller);
-
-  new ScrollMagic.Scene({
-    duration: offset,
-    offset: -1 * offset,
-  })
-  .setPin('#text-set-03')
-  .addTo(controller);
-
-  new ScrollMagic.Scene({
-    duration: offset,
-    offset: -1 * offset,
-  })
-  .setPin('#text-set-04')
-  .addTo(controller);
+  let controller = new ScrollMagic.Controller(),  
+      topWave = document.getElementById('validation-start'),
+      bottomWave = document.getElementById('validation-end'),
+      waveOffsetBegin = topWave.offsetTop + topWave.offsetHeight,
+      waveOffsetEnd = bottomWave.offsetTop,
+      imageHeight = document.getElementById('globe').offsetHeight;
+ 
+  document.getElementById('image-set-01').style.height = imageHeight + 'px'; 
 
   //---------------------------------------------------------
-  // Text Visibility Handlers
-  //---------------------------------------------------------
-  
-  /*new ScrollMagic.Scene({
-    duration: 0,
-    offset: 2000,
-  })
-  .setClassToggle('#text-set-02', 'invisible') 
-  .addTo(controller);
-
-  new ScrollMagic.Scene({ duration: 0,
-    offset: 3200,
-  })
-  .setClassToggle('#text-set-03', 'invisible')
-  .addTo(controller);
-
+  // Page Handling
+  //--------------------------------------------------------- 
   new ScrollMagic.Scene({
-    duration: 0,
-    offset: 4200,
+    duration: 2500,
+    offset: waveOffsetBegin,
   })
-  .setClassToggle('#text-set-04', 'invisible')
-  .addTo(controller);*/
-
-  new ScrollMagic.Scene({
-    duration: 800,
-    offset: offset + 1400,
-  })
-  .setClassToggle('#annotations', 'visible')
+  .setPin('#main')
   .addTo(controller);
-
-  
-  //---------------------------------------------------------
-  // Scaling Handlers
-  //---------------------------------------------------------
  
   new ScrollMagic.Scene({
+    duration: 3500,
+    offset: waveOffsetBegin *  5,
+  })
+  .setPin('.validation-container')
+  .reverse(true)
+  .addTo(controller);
+  
+  //---------------------------------------------------------
+  // text-set-02
+  //---------------------------------------------------------
+  new ScrollMagic.Scene({
     duration: 800,
-    offset: offset - 600,
+    offset: waveOffsetBegin,
+  })
+  .setClassToggle('#text-set-02', 'visible')
+  .addTo(controller);
+
+  new ScrollMagic.Scene({
+    duration: 800,
+    offset: waveOffsetBegin,
   })
   .setClassToggle('#people', 'scale-people-up')
   .addTo(controller);
 
+      //-----------------------------------------------------
+      // Transition out
+      //-----------------------------------------------------
+      new ScrollMagic.Scene({
+        duration: 0,
+        offset: waveOffsetBegin + 800,
+      })
+      .setClassToggle('.first-pieces', 'scale-pieces-up')
+      .addTo(controller);
+
+  //---------------------------------------------------------
+  // text-set-03
+  //---------------------------------------------------------
   new ScrollMagic.Scene({
-    duration: 0,
-    offset: offset + 500,
+    duration: 800,
+    offset: waveOffsetBegin + 800,
   })
-  .setClassToggle('.first-pieces', 'scale-pieces-up')
+  .setClassToggle('#text-set-03', 'visible')
   .addTo(controller);
 
   new ScrollMagic.Scene({
-    duration: 0,
-    offset: offset + 2597
+    duration: 800,
+    offset: waveOffsetBegin + 800,
   })
-  .setClassToggle('.next-pieces', 'scale-pieces-up')
+  .setClassToggle('#annotations', 'visible')
   .addTo(controller);
 
+      //-----------------------------------------------------
+      // Transition out
+      //-----------------------------------------------------
+      new ScrollMagic.Scene({
+        duration: 0,
+        offset: waveOffsetBegin + 1600
+      })
+      .setClassToggle('.next-pieces', 'scale-pieces-up')
+      .addTo(controller);
+
+  //---------------------------------------------------------
+  // text-set-04
+  //---------------------------------------------------------
   new ScrollMagic.Scene({
     duration: 0,
-    offset: offset + 3400,
+    offset: waveOffsetBegin + 1600,
+  })
+  .setClassToggle('#text-set-04', 'visible')
+  .addTo(controller);
+  
+  new ScrollMagic.Scene({
+    duration: 0,
+    offset: waveOffsetBegin + 1600,
   })
   .setClassToggle('#rays', 'scale-rays-up')
-  .addTo(controller);
-
-  
-
-
-  
-  
-  
+  .addTo(controller); 
 }
